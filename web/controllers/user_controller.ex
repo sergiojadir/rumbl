1857d9a1792cfabeb446d5135c3fc1e9abf59256
Sergio Jadir Ribeiro Leite <sergio_jadir@outlook.com>
2017-01-18 22:34:07 -0200

    Building forms

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -1,9 +1,15 @@
 defmodule Rumbl.UserController do
 	use Rumbl.Web, :controller
+	alias Rumbl.User
 
 	def index(conn, _params) do
 		users = Repo.all(Rumbl.User)
 		render conn, "index.html", users: users
+	end
+
+	def new(conn, _params) do
+		changeset = User.changeset(%User{})
+		render conn, "new.html", changeset: changeset
 	end
 
 	def show(conn, %{"id" => id}) do
