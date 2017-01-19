6edc61c57b45f88716516da53589f3ebae3d6c4f
Sergio Jadir Ribeiro Leite <sergio_jadir@outlook.com>
2017-01-18 22:55:47 -0200

    Creating resources.

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -12,6 +12,18 @@ defmodule Rumbl.UserController do
 		render conn, "new.html", changeset: changeset
 	end
 
+	def create(conn, %{"user" => user_params}) do
+		changeset = User.changeset(%User{}, user_params)
+		case Repo.insert(changeset) do
+			{:ok, user} ->
+				conn
+				|> put_flash(:info, "#{user.name} created!")
+				|> redirect(to: user_path(conn, :index))
+			{:error, changeset} ->
+				render(conn, "new.html", changeset: changeset)
+		end
+	end
+
 	def show(conn, %{"id" => id}) do
 		user = Repo.get(Rumbl.User, id)
 		render conn, "show.html", user: user
